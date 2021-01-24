5100da1dd49754e52b2a6107f5878f666440b8fe
Richi Zee <rz@zarix.de>
2021-01-24 21:06:57 +0100

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Kivitendo Docker
+
+## Version
+
+* 3.5.6.1
+
+## Installation
+
+### Build
+
+```
+git clone https://github.com/zarix/kivitendo_docker
+cp .env.template .env
+docker-compose build
+```
+
+### Start
+```
+docker-compose up -d
+```
+# Contribution
+
+If you want contribute to this project feel free to fork this project, do your work in a branch and create a pull request.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+cd /var/www/kivitendo-erp
+chown -R www-data:www-data webdav spool
+chown www-data templates users
+
+KIVI_CONFIG="config/kivitendo.conf"
+
+sed -i "s/\$ADMIN_PASSWORD/$ADMIN_PASSWORD/g" $KIVI_CONFIG
+sed -i "s/\$DB_HOST/$DB_HOST/g" $KIVI_CONFIG
+sed -i "s/\$DB_PORT/$DB_PORT/g" $KIVI_CONFIG
+sed -i "s/\$DB_NAME/$DB_NAME/g" $KIVI_CONFIG
+sed -i "s/\$DB_USER/$DB_USER/g" $KIVI_CONFIG
+sed -i "s/\$DB_PASSWORD/$DB_PASSWORD/g" $KIVI_CONFIG
+
+sed -i "s/\$AUTH_MODULE/$AUTH_MODULE/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_HOST/$LDAP_HOST/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_PORT/$LDAP_PORT/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_TLS/$LDAP_TLS/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_ATTR/$LDAP_ATTR/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_BASEDN/$LDAP_BASEDN/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_FILTER/$LDAP_FILTER/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_BINDDN/$LDAP_BINDDN/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_BINDPW/$LDAP_BINDPW/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_TIMEOUT/$LDAP_TIMEOUT/g" $KIVI_CONFIG
+sed -i "s/\$LDAP_VERIFY/$LDAP_VERIFY/g" $KIVI_CONFIG
+
+sed -i "s/\$MAIL_METHOD/$MAIL_METHOD/g" $KIVI_CONFIG
+sed -i "s/\$MAIL_HOST/$MAIL_HOST/g" $KIVI_CONFIG
+sed -i "s/\$MAIL_PORT/$MAIL_PORT/g" $KIVI_CONFIG
+sed -i "s/\$MAIL_SECURITY/$MAIL_SECURITY/g" $KIVI_CONFIG
+sed -i "s/\$MAIL_LOGIN/$MAIL_LOGIN/g" $KIVI_CONFIG
+sed -i "s/\$MAIL_PASSWORD/$MAIL_PASSWORD/g" $KIVI_CONFIG
+
+$@
